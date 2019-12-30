d183008cb02e4f922e9945837aab5b31d00f1daa
georgetegral <georgetegral@gmail.com>
2019-12-30 16:31:26 -0600

    Programa que toma 2 variables y las imprime

---
+++ utilityHost.sh
@@ -0,0 +1,9 @@
+# !/bin/bash
+# Programa que toma dos variables y las imprime
+# Autor: georgetegral
+
+option=$1
+result=$2
+
+echo "Valor de la variable option= $option"
+echo "Valor de la variable result= $result"
