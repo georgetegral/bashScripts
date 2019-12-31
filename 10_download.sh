d10c32a4c4c4e3ae9e06361806788f7dcf6890bb
georgetegral <georgetegral@gmail.com>
2019-12-31 14:07:46 -0600

    Script para ejemplificar la descarga de archivos con wget

---
+++ 10_download.sh
@@ -0,0 +1,6 @@
+# !/bin/bash
+# Programa para ejemplificar el uso de la descarga de información desde internet utilizando el comando wget 
+# Autor: georgetegral
+
+echo "Descargar información de internet"
+wget https://www-us.apache.org/dist/tomcat/tomcat-8/v8.5.50/bin/apache-tomcat-8.5.50.zip
