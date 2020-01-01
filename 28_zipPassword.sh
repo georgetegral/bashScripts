19ba2083249f06b44c2da41cfa3fc390cecbfa59
georgetegral <georgetegral@gmail.com>
2019-12-31 20:45:18 -0600

    Script para crear archivos con contraseña con zip

---
+++ 28_zipPassword.sh
@@ -0,0 +1,6 @@
+# !/bin/bash
+# Programa para ejemplificar el empaquetamiento con contraseña utilizando zip
+# Autor: georgetegral
+
+echo "Empaquetar todos los scripts de la carpeta y asignarle contraseña"
+zip -e shellCourse.zip *.sh
