da197f1bebbd76d352449dcd23623ff6b4697c32
georgetegral <georgetegral@gmail.com>
2019-12-31 20:37:59 -0600

    Scripts de empaquetamiento tar, gzip y pbzip2 con sus respectivos archivos

---
+++ 25_tar.sh
@@ -0,0 +1,6 @@
+# !/bin/bash
+# Programa para ejemplificar el empaquetamiento con el comando tar
+# Autor: georgetegral
+
+echo "Empaquetar todos los scripts de la carpeta"
+tar -cvf shellCourse.tar *.sh
